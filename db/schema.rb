434804d23fce4d8435eab8198c917540f68f337d
Nicolas Scholz <[Dnicscholz>
2015-12-22 09:17:18 -0500

    fixes some problems with password digest

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20151221133029) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "items", force: :cascade do |t|
